e893fb58b7061693f8827bcc3abe65ae8a267cdd
Installshi3ld <61904137+Installshi3ld@users.noreply.github.com>
2023-03-24 02:10:09 +0100

    Add Main Chara mesh

---
+++ Source/DevKitDoomLike/CPP_FunctionLibrary.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Math/UnrealMathUtility.h"
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CPP_FunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKITDOOMLIKE_API UCPP_FunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+
+};
+
+
